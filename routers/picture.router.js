f0bf44bad6052f71acdbb50f65f773a849e959ce
Trinh Dong <admin@dongdev.com>
2022-04-02 23:03:51 +0700

    Refactor: rename rename picture file

--- routers/picture.router.js
+++ routers/picture.router.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { uploadPicture, newPicture } from '../controllers/picture.controllers.js';
+import { uploadPicture, newPicture } from '../controllers/picture.controller.js';
 
 const router = express.Router();
 
